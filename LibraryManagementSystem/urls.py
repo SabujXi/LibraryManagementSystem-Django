16d4f674981aa7b90217241d015c6737282f6578
SabujXi <md.sabuj.sarker@gmail.com>
2019-09-13 16:58:41 +0600

    Updated gitignonre, refactored code for tabvs vs spaces

--- LibraryManagementSystem/urls.py
+++ LibraryManagementSystem/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 from django.http import HttpResponse
 
+
 def hello(request):
-	var = "<!doctype html>\n" + ""
-	return HttpResponse(var)
+    var = "<!doctype html>\n" + ""
+    return HttpResponse(var)
+
 
 def hello_file(request):
-	...
-	return HttpResponse(var)
+    ...
+    return HttpResponse(var)
+
 
 urlpatterns = [
-	path('a', hello),
-	#path('b', hello_file)
+    path('a', hello),
+    #path('b', hello_file)
 ]
