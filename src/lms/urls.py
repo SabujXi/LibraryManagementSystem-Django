c22ab8cac7223831124627421967bf5065531a9f
SabujXi <md.sabuj.sarker@gmail.com>
2019-09-17 18:39:45 +0600

    Django view & template; How django finds templates; How app templates work

--- src/lms/urls.py
+++ src/lms/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.http import HttpResponse
+from .views import home
 
 urlpatterns = [
+    path('', home),
     path('a', lambda x: HttpResponse('I am a'))
 ]
