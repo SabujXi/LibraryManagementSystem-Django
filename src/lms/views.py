a23674a89ef6dc2a896803ff85c11d6aefa9f317
SabujXi <md.sabuj.sarker@gmail.com>
2019-09-20 17:50:35 +0600

    Django template filter practice

--- src/lms/views.py
+++ src/lms/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     context = {
-        'title': 'My Horrible Awesome Homepage'
+
     }
     return render(req, "lms/base.html", context=context)
