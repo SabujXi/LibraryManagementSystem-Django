95ffee5e737eec1df2c262a20088a1a6b4600ecb
SabujXi <md.sabuj.sarker@gmail.com>
2019-09-20 17:43:19 +0600

    Added bootstrap, jquery; used them in our base template

--- src/LibraryManagementSystem/settings.py
+++ src/LibraryManagementSystem/settings.py
@@ -69,6 +69,10 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 WSGI_APPLICATION = 'LibraryManagementSystem.wsgi.application'
 
 
@@ -120,5 +124,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = r'D:\PythonDjango\test'
