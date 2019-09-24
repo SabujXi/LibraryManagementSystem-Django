4feaf5d970cc1d8509a1b86c976888a53a470a44
SabujXi <md.sabuj.sarker@gmail.com>
2019-09-24 18:46:38 +0600

    Added about and made fun with .php link

--- src/lms/urls.py
+++ src/lms/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from django.http import HttpResponse
-from .views import home
+from .views import home, about
 
 urlpatterns = [
     path('', home),
+    path('about.php', about),
     path('a', lambda x: HttpResponse('I am a'))
 ]
--- src/lms/views.py
+++ src/lms/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     context = {
-
     }
     return render(req, "lms/home.html", context=context)
+
+
+def about(req):
+    context = {
+    }
+    return render(req, "lms/about.html", context=context)
