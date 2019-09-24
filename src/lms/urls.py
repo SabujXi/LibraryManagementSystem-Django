d9af33e06c9e3de99f53297992f2b80daae19bfd
SabujXi <md.sabuj.sarker@gmail.com>
2019-09-24 19:07:52 +0600

    Added book list: view, templates, named route

--- src/lms/urls.py
+++ src/lms/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from django.http import HttpResponse
-from .views import home, about
+from .views import (
+    home,
+    about,
+    book_list
+)
 
 urlpatterns = [
     path('', home, name='home'),
     path('about.php', about, name='about'),
+    path('book-list.aspx', book_list, name='book-list'),
     # testing urls
     path('a', lambda x: HttpResponse('I am a'))
 ]
--- src/lms/views.py
+++ src/lms/views.py
@@ -11,3 +11,9 @@ def about(req):
     context = {
     }
     return render(req, "lms/about.html", context=context)
+
+def book_list(req):
+    context = {
+    }
+    return render(req, "lms/book_list.html", context=context)
+
