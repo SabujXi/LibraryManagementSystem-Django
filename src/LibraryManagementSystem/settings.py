dc1e724f5034cd2d1035d57ff2b9747930de7131
SabujXi <md.sabuj.sarker@gmail.com>
2019-09-27 17:21:01 +0600

    Book model created; mysql settings added; mysqlclient installed; made migration scripts; commtted migration; added data to database; tested app

--- src/LibraryManagementSystem/settings.py
+++ src/LibraryManagementSystem/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'LibraryManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lms',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 
--- src/lms/models.py
+++ src/lms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
