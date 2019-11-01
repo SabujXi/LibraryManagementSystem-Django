d53619f55d75cded34c306754404b71484d2d68e
Shishir <sishishir2015@gmail.com>
2019-11-01 17:00:25 +0600

    Background Color Remove

--- src/lms/models.py
+++ src/lms/models.py
@@ -18,11 +18,7 @@ class Member(models.Model):
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
-    sex = models.CharField(max_length=10, choices=(
-        "Male","Male",
-        "Female","Female",
-        "Others","Others"
-    ))
+    gender = models.CharField(max_length=255, choices=[("Male","Male"),("Female","Female"),("Others","Others")])
     birth_date = models.DateField(null=True)
     death_date = models.DateField(null=True)
     bio = models.TextField(null=True)
